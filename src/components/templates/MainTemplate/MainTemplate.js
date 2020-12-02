57311eaa9cbfe985bc7b6ddf9f6d329a67b6250e
ssymax <sszymass@gmail.com>
2020-12-02 20:24:25 +0100

    starter pack

---
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import GlobalStyle from 'assets/styles/GlobalStyle';
+import { theme } from 'assets/styles/theme';
+import { ThemeProvider } from 'styled-components';
+import Helmet from 'react-helmet';
+
+const MainTemplate = ({ children }) => {
+  return (
+    <>
+      <Helmet lang="pl" title="hello simon" />
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </>
+  );
+};
+
+export default MainTemplate;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const IndexPage = () => (
+  <div>
+    <h1>hello simon</h1>
+  </div>
+)
+
+export default IndexPage
